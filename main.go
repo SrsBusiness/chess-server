4b07cd459f244b53fd97de64a243c55a464816a3
Danny Chen <dannychen579@utexas.edu>
2022-04-16 20:52:30 -0700

    first commit

---
+++ main.go
@@ -0,0 +1,32 @@
+package chess_server
+
+import (
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"net/http"
+)
+
+func main() {
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Routes
+	e.GET("/", hello)
+	e.GET("/find_match", find_match)
+
+	// Start server
+	e.Logger.Fatal(e.Start(":1323"))
+}
+
+// Handler
+func hello(c echo.Context) error {
+	return c.String(http.StatusOK, "Hello, World!")
+}
+
+func find_match(c echo.Context) error {
+	return c.String(http.StatusOK, "Finding Match")
+}
