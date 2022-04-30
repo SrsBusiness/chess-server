8ec91395b71812c5f19947556b5c3b1e85707c5c
Danny Chen <dchenviolinist@gmail.com>
2022-04-30 14:34:05 -0500

    "Playing a game over websocket. Garbage AI"

--- chess_server/handlers.go
+++ chess_server/handlers.go
@@ -3,7 +3,11 @@ package chess_server
 import (
 	"net/http"
 
+	"strings"
+
 	"github.com/labstack/echo/v4"
+	"github.com/notnil/chess"
+	"golang.org/x/net/websocket"
 )
 
 // Handler
@@ -12,5 +16,59 @@ func Hello(c echo.Context) error {
 }
 
 func FindMatch(c echo.Context) error {
-	return c.String(http.StatusOK, "Finding Match")
+	//return c.String(http.StatusOK, "Finding Match")
+	websocket.Handler(func(ws *websocket.Conn) {
+		defer ws.Close()
+		game := chess.NewGame()
+		var err error
+		for game.Outcome() == chess.NoOutcome {
+			// Player is white
+			msg := ""
+			err = websocket.Message.Receive(ws, &msg)
+			if err != nil {
+				c.Logger().Error(err)
+				continue
+			}
+
+			if err = game.MoveStr(strings.TrimSpace(msg)); err != nil {
+				c.Logger().Error(err)
+				continue
+			}
+
+			if game.Outcome() != chess.NoOutcome {
+				break
+			}
+			moves := game.ValidMoves()
+
+			move := moves[0]
+			game.Move(move)
+
+			err = websocket.Message.Send(ws, move.String())
+			if err != nil {
+				c.Logger().Error(err)
+			}
+
+			err = websocket.Message.Send(ws, game.Position().Board().Draw())
+			if err != nil {
+				c.Logger().Error(err)
+			}
+		}
+		err = websocket.Message.Send(ws, game.Position().Board().Draw())
+		if err != nil {
+			c.Logger().Error(err)
+		}
+		end_msg := ""
+		if game.Outcome() == chess.WhiteWon {
+			end_msg = "You win!"
+		} else if game.Outcome() == chess.Draw {
+			end_msg = "Draw!"
+		} else {
+			end_msg = "You lose!"
+		}
+		err = websocket.Message.Send(ws, end_msg)
+		if err != nil {
+			c.Logger().Error(err)
+		}
+	}).ServeHTTP(c.Response(), c.Request())
+	return nil
 }
